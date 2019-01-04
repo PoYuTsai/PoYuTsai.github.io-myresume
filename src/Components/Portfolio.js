876c1cea54a67c592ae34a15a7164213c5a9de0c
蔡柏裕 <bobotsai1204@Eric.local>
2019-01-04 23:48:54 +0800

    fix work screen and paper

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -30,11 +30,8 @@ class Portfolio extends Component {
       <section id="portfolio">
 
       <div className="row">
-
          <div className="twelve columns collapsed">
-
             <h1>作品們</h1>
-
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
             </div>
--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -2,38 +2,15 @@ import React, { Component } from 'react';
 
 class Testimonials extends Component {
   render() {
-
-    if(this.props.data){
-      var testimonials = this.props.data.testimonials.map(function(testimonials){
-        return  <li key={testimonials.user}>
-            <blockquote>
-               <p>{testimonials.text}</p>
-               <cite>{testimonials.user}</cite>
-            </blockquote>
-         </li>
-      })
-    }
-
     return (
-      <section id="testimonials">
-      <div className="text-container" style={{height:'100vh'}}>
-         <div className="row" style={{height:'100%'}}>
-
-            <div className="two columns header-col" style={{margin:'0% 0 0 0'}}>
-              <ul className="slides">
-                {testimonials}
-              </ul>
-            </div>
-
-            <div className="container" id="thesis" style={{height:'100%'}}>
-              <iframe title="paper" style={{ width:'70%', height: '100%' }}
-                src="https://drive.google.com/file/d/10yGI-PXpleuYBUBeolZ1w1MFMTZ7s_NV/preview">
-              </iframe> 
-            </div>
-
-          </div>
+      <div className="text-container" styles="margin-top:30px">
+         <div className="row" styles="height:29.7cm">
+          <h2 className="my-4">碩士論文</h2>
+            <iframe title="paper" style={{ width:'100%', height: '26cm' }}
+              src="https://drive.google.com/file/d/10yGI-PXpleuYBUBeolZ1w1MFMTZ7s_NV/preview">
+            </iframe> 
         </div>
-   </section>
+      </div>
     );
   }
 }
